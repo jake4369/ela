a275cab4557353264e7d58ea3fd9482e2673771e
Jake Samuels <samuels242@googlemail.com>
2024-10-17 12:57:44 +0100

    Temp change to header

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,6 +1,30 @@
 import { ParallaxScroll } from "@/components/ui/parallax-scroll";
 import LampDemo from "@/components/ui/lamp";
 
+import { Spotlight } from "@/components/ui/spotlight";
+
+export function SpotlightPreview() {
+  return (
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Spotlight <br /> is the new trend.
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          Spotlight effect is a great way to draw attention to a specific part
+          of the page. Here, we are drawing the attention towards the text
+          section of the page. I don&apos;t know why but I&apos;m running out of
+          copy.
+        </p>
+      </div>
+    </div>
+  );
+}
+
 const images = [
   "/assets/images/gallery/1.jpg",
   "/assets/images/gallery/2.jpg",
@@ -33,7 +57,10 @@ const images = [
 const Page = () => {
   return (
     <div className="mb-80">
-      <LampDemo>Gallery</LampDemo>
+      <LampDemo>
+        <h1 className="mb-2">Gallery</h1>
+        <p className="text-sm">Scroll down to see our work</p>
+      </LampDemo>
       <ParallaxScroll images={images} />
     </div>
   );
