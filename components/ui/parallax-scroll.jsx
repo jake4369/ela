4766a571a81c00b72d93a11e6bd707a435aa4a35
Jake Samuels <samuels242@googlemail.com>
2024-10-17 14:33:40 +0100

    Parallax scroll

--- components/ui/parallax-scroll.jsx
+++ components/ui/parallax-scroll.jsx
@@ -5,10 +5,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
-export const ParallaxScroll = ({
-  images,
-  className
-}) => {
+export const ParallaxScroll = ({ images, className }) => {
   const gridRef = useRef(null);
   const { scrollYProgress } = useScroll({
     container: gridRef, // remove this if your container is not fixed height
@@ -26,24 +23,28 @@ export const ParallaxScroll = ({
   const thirdPart = images.slice(2 * third);
 
   return (
-    (<div
+    <div
       className={cn("h-[40rem] items-start overflow-y-auto w-full", className)}
-      ref={gridRef}>
+      ref={gridRef}
+    >
       <div
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start  max-w-5xl mx-auto gap-10 py-40 px-10"
-        ref={gridRef}>
+        ref={gridRef}
+      >
         <div className="grid gap-10">
           {firstPart.map((el, idx) => (
             <motion.div
               // Apply the translateY motion value here
               style={{ y: translateFirst }}
-              key={"grid-1" + idx}>
+              key={"grid-1" + idx}
+            >
               <Image
                 src={el}
                 className="h-80 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
                 height="400"
                 width="400"
-                alt="thumbnail" />
+                alt="thumbnail"
+              />
             </motion.div>
           ))}
         </div>
@@ -55,7 +56,8 @@ export const ParallaxScroll = ({
                 className="h-80 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
                 height="400"
                 width="400"
-                alt="thumbnail" />
+                alt="thumbnail"
+              />
             </motion.div>
           ))}
         </div>
@@ -67,11 +69,12 @@ export const ParallaxScroll = ({
                 className="h-80 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
                 height="400"
                 width="400"
-                alt="thumbnail" />
+                alt="thumbnail"
+              />
             </motion.div>
           ))}
         </div>
       </div>
-    </div>)
+    </div>
   );
 };
