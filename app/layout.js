b802dbd1b515d282e16b6939ea7eb54bc9fdcd27
Jake Samuels <samuels242@googlemail.com>
2024-10-22 11:39:06 +0100

    Shorten description

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,11 @@ import { CenteredWithLogo } from "@/components/ui/footer";
 export const metadata = {
   title: "ELA Pressure Washing",
   description:
-    "ELA Pressure Washing provides top-quality pressure washing, steam cleaning, and surface restoration services for residential, commercial, and heritage properties. Our expert team specializes in high-pressure washing, soft washing, chemical treatments, roof cleaning, and more, ensuring your property looks its best and stays protected. Whether you need graffiti removal, driveway leveling, or patio re-pointing, we deliver outstanding results with eco-friendly solutions and professional care.",
+    "ELA Pressure Washing offers professional pressure washing, steam cleaning, and surface restoration for residential, commercial, and heritage properties. Eco-friendly solutions.",
   keywords:
     "pressure washing, steam cleaning, heritage site cleaning, high pressure wash, soft wash, chemical treatments, sealing, roof cleaning, biocide treatments, driveway lifting, patio re-pointing, graffiti removal, chewing gum removal, residential, commercial,",
   author: "ELA Pressure Washing",
   robots: "index, follow",
-  // viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({ children }) {
@@ -21,7 +20,6 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta name="author" content={metadata.author} />
-        {/* <meta name="viewport" content={metadata.viewport} /> */}
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
@@ -31,6 +29,7 @@ export default function RootLayout({ children }) {
         />
         <meta property="og:image" content="/assets/opengraph.png" />
         <meta property="og:site_name" content="ELA Pressure Washing" />
+        <meta name="twitter:image" content="/assets/opengraph.png" />
 
         <link rel="icon" href="/assets/favicon.ico" />
       </head>
