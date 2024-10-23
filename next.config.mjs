6446036ac427d4906a83871506a42b696e0befc2
Jake Samuels <samuels242@googlemail.com>
2024-10-23 15:56:28 +0100

    Clear config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    EMAIL_USERNAME: "jsamuelsdev@gmail.com",
-    SITE_MAIL_RECIEVER: "jsamuelsdev@gmail.com",
-    EMAIL_PASSWORD: "Lilbrett1!",
-    EMAIL_HOST: "smtp.gmail.com",
-    EMAIL_PORT: 465,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
