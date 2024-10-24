01672d71b154a29241e09d0f45c6e25ba85ff7c7
Jake Samuels <samuels242@googlemail.com>
2024-10-24 13:08:44 +0100

    Email template

--- components/emails/Email.jsx
+++ components/emails/Email.jsx
@@ -1,26 +1,93 @@
-import { Button, Html, Img, Link } from "@react-email/components";
+import { Html, Heading, Img, Link } from "@react-email/components";
 import * as React from "react";
 
-export default function Email({ name, email, phone, contactMethod, message }) {
+export default function Email({
+  name,
+  email = "email@gmail.com",
+  phone = "01902656564",
+  contactMethod,
+  message,
+}) {
   return (
     <Html>
-      <h1>ELA Pressure Washing</h1>
+      <body
+        style={{
+          fontFamily: "sans-serif",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <main
+          style={{
+            margin: "0 auto",
+            display: "flex",
+            flexDirection: "column",
+            width: "87.2%",
+            maxWidth: "689px",
+          }}
+        >
+          <h1
+            style={{
+              textAlign: "center",
+              color: "#005080",
+            }}
+          >
+            ELA Pressure Washing And Steam Cleaning
+          </h1>
 
-      <p>
-        You have contact request from <strong>{name}</strong>
-      </p>
+          <p
+            style={{
+              color: "#484848",
+            }}
+          >
+            You have contact request from:
+            <strong
+              style={{ display: "block", color: "#000", marginTop: "5px" }}
+            >
+              {name}
+            </strong>
+          </p>
 
-      <h2>Message</h2>
-      <p>{message}</p>
+          <h2>Message</h2>
+          <div
+            style={{
+              padding: "24px",
+              backgroundColor: "#f2f3f3",
+              borderRadius: "4px",
+            }}
+          >
+            <quote>"{message}"</quote>
+          </div>
 
-      <h3>Preferred method of contact:</h3>
-      <p>
-        {contactMethod.phone && contactMethod.email
-          ? `${email} or ${phone}`
-          : contactMethod.phone
-          ? phone
-          : email}
-      </p>
+          <h3>Preferred method of contact:</h3>
+
+          <p>
+            {contactMethod.phone && contactMethod.email ? (
+              `Email: ${email} or Phone: ${phone}`
+            ) : contactMethod.phone ? (
+              <>
+                Phone: <Link href={`tel:${phone}`}>{phone}</Link>
+              </>
+            ) : contactMethod.email ? (
+              <>
+                Email: <Link href={`mailto:${email}`}>{email}</Link>
+              </>
+            ) : (
+              "No contact information available"
+            )}
+          </p>
+
+          <Img
+            src="https://i.ibb.co/drmcvWK/logo.png"
+            alt="ELA logo"
+            width="250"
+            height="250"
+            style={{
+              alignSelf: "center",
+            }}
+          />
+        </main>
+      </body>
     </Html>
   );
 }
